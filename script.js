d2520664eb1b2355c5843e280e333183b2b61706
William Abrokwah <abrokwah27e@gmail.com>
2025-07-24 16:13:11 -0400

    allow value of a cell to be changed by numpad buttons

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const cells = document.querySelectorAll('.sudoku-cell');
-let selectedButton = null;
+let selectedCell = null;
 
 cells.forEach(cell => {
   cell.addEventListener('click', () => {
     cells.forEach(c => c.classList.remove('selected'));
     cell.classList.add('selected');
+    selectedCell = cell;
+  });
+});
+
+const numpadButtons = document.querySelectorAll('.numpad-item');
+
+numpadButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    if (selectedCell) {
+      selectedCell.textContent = button.textContent;
+    }
   });
 });
